5fb8c56d9d4400cb6fab83141f75cba09d559e9a
asilyanur <lizar5413@gmail.com>
2025-11-22 09:14:32 +0300

    Перезапись main

--- lab2.01/src/main/java/Main.java
+++ lab2.01/src/main/java/Main.java
@@ -3,6 +3,16 @@
 */
 public class Main {
     public static void main(String[] args) {
-        new Program().run();
+        Program program = new Program();
+        User user = program.signIn();
+
+        if (user == null) {
+            System.out.println("Вы ввели неверный логин или пароль!");
+        } else {
+            System.out.println("Добро пожаловать!");
+
+            Work work = new Work(user);
+            work.showMenu();
+        }
     }
 }
