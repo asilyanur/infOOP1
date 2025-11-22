c206862a36bbd3ace57a1189ec46537636c2191f
asilyanur <lizar5413@gmail.com>
2025-11-22 09:08:49 +0300

    Revert "Добавлен main"
    This reverts commit 45d6afb6798f3b387b71398ffe849f72cea6ed2e.

--- lab2.01/src/main/java/Main.java
+++ lab2.01/src/main/java/Main.java
@@ -3,16 +3,6 @@
 */
 public class Main {
     public static void main(String[] args) {
-        Program program = new Program();
-        User user = program.signIn();
-
-        if (user == null) {
-            System.out.println("Вы ввели неверный логин или пароль!");
-        } else {
-            System.out.println("Добро пожаловать!");
-
-            Work work = new Work(user);
-            work.showMenu();
-        }
+        new Program().run();
     }
 }
