1f3add7df317fb271af62127ea326df76bdf5a91
asilyanur <lizar5413@gmail.com>
2025-11-22 09:08:43 +0300

    Revert "Добавлены scanner, showAdminMenu, showUserMenu"
    This reverts commit 06438c008361805b1daeed3db1f8fa2653704108.

--- lab2.01/src/main/java/Work.java
+++ lab2.01/src/main/java/Work.java
@@ -1,55 +1,17 @@
-import java.util.Scanner;
-
 public class Work {
 
     private User user;
-    private Scanner scanner;
 
     public Work(User user) {
         this.user = user;
-        this.scanner = new Scanner(System.in);
     }
 
     private void showAdminMenu() {
-        while (true) {
-            System.out.println("1 - Посмотреть информацию о себе");
-            System.out.println("2 - Выйти");
-            System.out.print("Выберите действие: ");
-            String choice = scanner.nextLine();
 
-            switch (choice) {
-                case "1":
-                    System.out.println("Логин: " + user.getLogin());
-                    System.out.println("Роль: " + user.getRole());
-                    break;
-                case "2":
-                    System.out.println("Выход из меню администратора.");
-                    return;
-                default:
-                    System.out.println("Неверный выбор. Попробуйте снова.");
-            }
-        }
     }
 
     private void showUserMenu() {
-        while (true) {
-            System.out.println("1 - Посмотреть информацию о себе");
-            System.out.println("2 - Выйти");
-            System.out.print("Выберите действие: ");
-            String choice = scanner.nextLine();
 
-            switch (choice) {
-                case "1":
-                    System.out.println("Логин: " + user.getLogin());
-                    System.out.println("Роль: " + user.getRole());
-                    break;
-                case "2":
-                    System.out.println("Выход из меню пользователя.");
-                    return;
-                default:
-                    System.out.println("Неверный выбор. Попробуйте снова.");
-            }
-        }
     }
 
     public void showMenu() {
